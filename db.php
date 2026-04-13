506995f235dd8ca12dd9f56b0c9b97dfc6d1efad
mary-220493 <mary.220493@college.edu>
2026-04-13 16:20:12 +0530

    mongodb connection

--- db.php
+++ db.php
@@ -1,15 +1,22 @@
-<?php
+''''
 // Simple Database Connection Script
-$host = "localhost";
+'''$host = "localhost";
 $user = "root";
 $pass = "";
 $db_name = "streetzen_db";
 
 // Connect to MySQL
-$conn = mysqli_connect($host, $user, $pass, $db_name);
+//$conn = mysqli_connect($host, $user, $pass, $db_name);
 
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+//if (!$conn) {
+    //die("Connection failed: " . mysqli_connect_error());
+//}'''
+<?php
+$conn = mysqli_connect("localhost","root","","userdb");
+if(!$conn) {
+die("Database connection failed");
 }
+echo "Database connected succesfully";
+?>
 ?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<form action="login_process.php" method="post">
+  <h2 style="text-align:center;">Login</h2>
+<h2>Login</h2>
+
+<input type="text" name="username" placeholder="Enter Username" required>
+<input type="password" name="password" placeholder="Enter Password" required>
+
+  <label>Username</label>
+  <input type="text" placeholder="Enter Username" required>
+<button type="submit">Login</button>
+
+  <label>Password</label>
+  <input type="password" placeholder="Enter Password" required>
+<p>Don’t have an account? <a href="signup.php">Sign Up</a></p>
+
+  <button type="submit">Login</button>
+  <p class="signup">
+    Don’t have an account? <a href="signup.html">Sign up</a>
+  </p>
+</form>
+
+</body>
+</html>
